cfd16965ede3d0f801815464bf7481348a5ee9a7
Harrior <harrior@users.noreply.github.com>
2021-08-27 19:55:12 +0300

    feat: added hotkeys

--- custom-video-player/js/script.js
+++ custom-video-player/js/script.js
@@ -12,6 +12,7 @@ const screenPlayButton = player.querySelector('.player__on_screen_play');
 const fullscreenButton = player.querySelector('.player__fullscreen');
 
 let volumeLevel = Number(volumeBar.value);
+let playbackRate = video.playbackRate * 100;
 
 window.addEventListener('load', function () {
     updateBarPosition(progressBar, progressBar.value);
@@ -31,101 +32,171 @@ video.addEventListener('click', () => {
     switchPlayback();
 })
 
-video.addEventListener('ended',function (){
+video.addEventListener('ended', function () {
     stopPlayback();
 })
 
 // playback position listeners
-progressBar.addEventListener('input', function() {
+progressBar.addEventListener('input', function () {
     setVideoPosition(progressBar.value)
 })
 
-video.addEventListener('timeupdate', function (){
-    updateBarPosition(progressBar,100/video.duration *  video.currentTime);
+video.addEventListener('timeupdate', function () {
+    updateBarPosition(progressBar, 100 / video.duration * video.currentTime);
 })
 
 // sound listeners
-muteButton.addEventListener('click', function (){
-    if (video.muted) {
-        unmute();
-    } else {
-        mute();
-    }
+muteButton.addEventListener('click', function () {
+    switchMute();
 })
 
-volumeBar.addEventListener('input', function (){
-    const volumeLevel = Number(volumeBar.value);
-    if (volumeLevel === 0){
+volumeBar.addEventListener('input', function () {
+    const level = Number(volumeBar.value);
+    if (level === 0) {
         mute();
-    }
-    else {
+    } else {
         unmute();
-        setVolumeLevel(volumeLevel);
+        setVolumeLevel(level);
     }
 })
 
 // fullscreen listener
-fullscreenButton.addEventListener('click', function (){
-    if (document.fullscreenElement === player){
-        document.exitFullscreen();
-    } else {
-        player.requestFullscreen();
-    }
+fullscreenButton.addEventListener('click', function () {
+    switchFullscreen();
+})
 
+// keyboard listener
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case ' ' :
+            e.preventDefault();
+            switchPlayback();
+            break;
+        case 'f':
+            switchFullscreen();
+            break;
+        case 'm':
+            switchMute();
+            break;
+        case 'Home':
+            setVideoPosition(0);
+            break;
+        case 'End':
+            setVideoPosition(100);
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            setVideoPosition(Number(e.key * 10));
+            break;
+        case "ArrowUp":
+            unmute();
+            setVolumeLevel(volumeLevel + 10)
+            break;
+        case "ArrowDown":
+            unmute();
+            setVolumeLevel(volumeLevel - 10)
+            break;
+        case "<":
+            setPlaybackRate(playbackRate - 10);
+            break;
+        case ">":
+            setPlaybackRate(playbackRate + 10);
+            break;
+        case "/":
+            setPlaybackRate(100);
+            break;
+        default:
+            console.log(e)
+    }
 })
 
 // start/stop functions
-function switchPlayback(){
+function switchPlayback() {
     if (video.paused) {
         startPlayback();
-    }
-    else {
+    } else {
         stopPlayback();
     }
 }
 
-function startPlayback(){
+function startPlayback() {
     video.play();
     playButton.classList.add('player__pause');
     screenPlayButton.style.display = 'none';
 }
-function stopPlayback(){
+
+function stopPlayback() {
     video.pause();
     playButton.classList.remove('player__pause');
     screenPlayButton.style.display = 'block';
 }
 
 // audio functions
-function mute(){
+function switchMute() {
+    if (video.muted) {
+        unmute();
+    } else {
+        mute();
+    }
+}
+
+function mute() {
     video.muted = true;
     volumeLevel = Number(volumeBar.value);
     muteButton.classList.add('player__mute__on');
     updateBarPosition(volumeBar, 0)
 }
 
-function unmute(){
+function unmute() {
     video.muted = false;
     muteButton.classList.remove('player__mute__on');
     setVolumeLevel(volumeLevel)
 }
 
 function setVolumeLevel(level) {
+    level = Number(level);
+    level = level > 100 ? 100 : level < 0 ? 0 : level;
     volumeLevel = level;
-    video.volume = level/100;
+    video.volume = level / 100;
     volumeLevel.value = level;
     updateBarPosition(volumeBar, level)
 }
 
 // video position functions
-function setVideoPosition(position){
-    progressBar.value = position;
-    video.currentTime = video.duration/100 * position;
+function setVideoPosition(position) {
+    position = Number(position);
+    position = position > 100 ? 100 : position < 0 ? 0 : position;
+    video.currentTime = video.duration / 100 * position;
     updateBarPosition(progressBar, position);
 }
 
+//fullscreen
+function switchFullscreen() {
+    if (document.fullscreenElement === player) {
+        document.exitFullscreen();
+    } else {
+        player.requestFullscreen();
+    }
+}
+
+// set playback speed
+function setPlaybackRate(rate){
+    rate = Number(rate);
+    rate = rate < 0 ? 0 : rate;
+    playbackRate = rate;
+    video.playbackRate = rate/100;
+}
+
 // progress bar update
-function updateBarPosition(bar, position){
-    position = Math.floor(position)
-    bar.style.background = `linear-gradient(to right, var(--progress-bar-full) 0%, var(--progress-bar-full) ${position}%, var(--progress-bar-empty) ${position}%, var(--progress-bar-empty) 100%)`
+function updateBarPosition(bar, position) {
+    position = Math.floor(position);
+    bar.style.background = `linear-gradient(to right, var(--progress-bar-full) 0%, var(--progress-bar-full) ${position}%, var(--progress-bar-empty) ${position}%, var(--progress-bar-empty) 100%)`;
     bar.value = position;
 }
