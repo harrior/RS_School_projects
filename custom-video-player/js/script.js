cb94ac3a2252509cfa38296272487a9cbfe17f16
Harrior <harrior@users.noreply.github.com>
2021-08-27 17:28:32 +0300

    refactor: made little refactor

--- custom-video-player/js/script.js
+++ custom-video-player/js/script.js
@@ -1,56 +1,116 @@
+// init control components
 const player = document.querySelector('.player');
+
 const video = player.querySelector('.player__screen');
+
 const progressBar = player.querySelector('.player__progress-bar');
-const progressVolume = player.querySelector('.player__volume');
-const playerPlayButton = player.querySelector('.player__play');
-const playerOnScreenPlay = player.querySelector('.player__on_screen_play');
-const playerVolumeLevel = player.querySelector('.player__volume')
+const volumeBar = player.querySelector('.player__volume')
 
+const playButton = player.querySelector('.player__play');
+const muteButton = player.querySelector('.player__mute');
+const screenPlayButton = player.querySelector('.player__on_screen_play');
+const fullscreenButton = player.querySelector('.player__on_screen_play');
 
+let volumeLevel = Number(volumeBar.value);
 
-progressBar.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+// set play\pause listeners
+playButton.addEventListener('click', () => {
+    switchPlayback();
 })
 
-progressVolume.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+screenPlayButton.addEventListener('click', () => {
+    switchPlayback();
 })
 
-playerVolumeLevel.addEventListener('input', function () {
-    setVideoVolume(this.value);
-})
-
-playerPlayButton.addEventListener('click', () => {
-    toggleVideo();
-})
-playerOnScreenPlay.addEventListener('click', () => {
-    toggleVideo();
-})
 video.addEventListener('click', () => {
-    toggleVideo();
+    switchPlayback();
 })
 
+video.addEventListener('ended',function (){
+    stopPlayback();
+})
 
-function toggleVideo(){
-    if (video.paused) {
-        video.play();
-        playerPlayButton.classList.toggle('player__pause');
-        playerOnScreenPlay.style.display = 'none';
+// playback position listeners
+progressBar.addEventListener('input', function() {
+    setVideoPosition(progressBar.value)
+})
+
+video.addEventListener('timeupdate', function (){
+    updateProgressBarPosition(progressBar,100/video.duration *  video.currentTime);
+})
+
+// sound listeners
+muteButton.addEventListener('click', function (){
+    if (video.muted) {
+        unmute();
+    } else {
+        mute();
+    }
+})
+
+volumeBar.addEventListener('input', function (){
+    const volumeLevel = Number(volumeBar.value);
+    if (volumeLevel === 0){
+        mute();
     }
     else {
-        video.pause();
-        playerPlayButton.classList.toggle('player__pause');
-        playerOnScreenPlay.style.display = 'block';
+        unmute();
+        setVolumeLevel(volumeLevel);
+    }
+})
+
+// start/stop functions
+function switchPlayback(){
+    if (video.paused) {
+        startPlayback();
+    }
+    else {
+        stopPlayback();
     }
 }
 
-function setVideoVolume(level, mute=false){
-    if (mute) {
-        video.volume = 0;
-    }
-    else {
-        video.volume = level/100;
-    }
+function startPlayback(){
+    video.play();
+    playButton.classList.add('player__pause');
+    screenPlayButton.style.display = 'none';
+}
+function stopPlayback(){
+    video.pause();
+    playButton.classList.remove('player__pause');
+    screenPlayButton.style.display = 'block';
+}
+
+// audio functions
+function mute(){
+    video.muted = true;
+    volumeLevel = Number(volumeBar.value);
+    muteButton.classList.add('player__mute__on');
+    updateProgressBarPosition(volumeBar, 0)
+}
+
+function unmute(){
+    video.muted = false;
+    muteButton.classList.remove('player__mute__on');
+    setVolumeLevel(volumeLevel)
+}
+
+function setVolumeLevel(level) {
+    volumeLevel = level;
+    video.volume = level/100;
+    volumeLevel.value = level;
+    updateProgressBarPosition(volumeBar, level)
+}
+
+// video position functions
+function setVideoPosition(position){
+    progressBar.value = position;
+    video.currentTime = video.duration/100 * position;
+    updateProgressBarPosition(progressBar, position);
+}
+
+// progress bar update
+function updateProgressBarPosition(bar, position){
+    position = Math.floor(position)
+    bar.style.background = `linear-gradient(to right, var(--progress-bar-full) 0%, var(--progress-bar-full) ${position}%, var(--progress-bar-empty) ${position}%, var(--progress-bar-empty) 100%)`
+    bar.value = position;
 }
