0957ea7f6618abcd000ac5edb848f7b34eb41317
Harrior <harrior@users.noreply.github.com>
2021-09-24 21:15:45 +0300

    feat: added video.js and stylized progressbar and  playlist

---
+++ museum/js/video.js
@@ -0,0 +1,12 @@
+const playerProgress = document.querySelector('.player__progress');
+const playerVolume = document.querySelector('.player__volume');
+
+playerProgress.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
+
+playerVolume.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
