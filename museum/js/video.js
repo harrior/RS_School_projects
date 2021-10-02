584a4d162d4b56861766c745bf940cc174779481
Harrior <harrior@users.noreply.github.com>
2021-10-02 16:53:18 +0300

    feat: complete PLAYER section for 420px resolution

--- museum/js/video.js
+++ museum/js/video.js
@@ -1,5 +1,6 @@
 const playerProgress = document.querySelector('.player__progress');
 const playerVolume = document.querySelector('.player__volume');
+const playerScreen = document.querySelector('.player__video')
 
 playerProgress.addEventListener('input', function() {
     const value = this.value;
@@ -10,3 +11,11 @@ playerVolume.addEventListener('input', function() {
     const value = this.value;
     this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 })
+
+function playerResize() {
+    console.log(playerScreen.offsetWidth)
+    playerScreen.style.height = `${playerScreen.offsetWidth * 0.4513}px`
+}
+playerResize()
+
+new ResizeObserver(playerResize).observe(playerScreen)
