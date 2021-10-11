0b4fbe3ef67fe311254ef05f03a1415ccb35f2d0
Harrior <harrior@users.noreply.github.com>
2021-10-11 13:29:26 +0300

    feat: added gallery scroll animation

--- museum-dom/js/gallery.js
+++ museum-dom/js/gallery.js
@@ -1,16 +1,53 @@
-function makeImage(n){
-    let imgTemplate = document.querySelector('#gallery__image-template').content.cloneNode(true);
-    let img = imgTemplate.querySelector('img');
-    // img.src = `assets/img/galery/galery${n}.jpg`;
-    img.src = `assets/img/galery/galery${n}.webp`;
-    img.alt = `galery${n}`;
-    return imgTemplate;
+galleryInit()
+
+function galleryInit() {
+
+    let container = document.querySelector('.gallery__inner-wrapper')
+    let images = []
+    for (let i = 1; i <= 15; i++) {
+        images.push(makeImage(i))
+    }
+
+    images.sort(() => Math.random() - 0.5);
+    images.map((el) => {
+        container.append(el)
+    })
+
+    function makeImage(n) {
+        let imgTemplate = document.querySelector('#gallery__image-template').content.cloneNode(true);
+        let img = imgTemplate.querySelector('img');
+        img.src = `assets/img/galery/galery${n}.webp`;
+        img.alt = `galery${n}`;
+        return imgTemplate;
+    }
+
 }
 
-let container = document.querySelector('.gallery__inner-wrapper')
-let images = []
-for (let i=1; i<=15; i++){
-    images.push(makeImage(i))
+function isVisible(elem) {
+    let coords = elem.getBoundingClientRect();
+    let windowHeight = document.documentElement.clientHeight;
+    return coords.top > 0 && coords.top < windowHeight;
 }
-images.sort(() => Math.random() - 0.5);
-images.map((el) => {container.append(el)})
+
+function showVisible() {
+    let images = document.querySelectorAll('.gallery__image-container')
+    for (let img of images) {
+        if (isVisible(img)) {
+            img.classList.add('gallery__image-show')
+        }
+    }
+
+    if (images[0].classList.contains('gallery__image-show')) {
+        const gallery = document.querySelector('#gallery')
+        let coords = gallery.getBoundingClientRect();
+        if (coords.top - document.documentElement.clientHeight > 0) {
+            for (let img of images) {
+                img.classList.remove('gallery__image-show')
+            }
+        }
+    }
+
+    // return coords.top > 0 && coords.top < windowHeight;
+}
+
+window.addEventListener('scroll', showVisible)
