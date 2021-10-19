df18929cc8e4fa33abb4de911b36f6c31415fc1c
Harrior <harrior@users.noreply.github.com>
2021-10-19 19:51:28 +0300

    feat: add getTimesOfDay function

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -32,3 +32,17 @@ function updateDateTime(){
 
     dateString.textContent = currentDate
 }
+
+export function getTimesOfDay(){
+    const hour = new Date().getHours();
+
+    if ((hour >= 6) && (hour < 12)){
+        return "morning";
+    } else if ((hour >= 12) && (hour < 18)) {
+        return "afternoon";
+    } else if ((hour >= 18) && (hour <= 23)) {
+        return "evening";
+    } else {
+        return "night";
+    }
+}
