8b2b97c6bbb8fd1f63bd3bdb627974d4e2e748d5
Harrior <harrior@users.noreply.github.com>
2021-10-19 19:53:39 +0300

    feat: add wallpaper module

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,9 +1,14 @@
 import * as Clock from './clock.js';
 import * as Greeting from './greeting.js';
+import * as Wallpapers from './wallpapers.js';
 
 localStorage.setItem('lang', 'en'); // TODO replace debug value
+localStorage.setItem('imgSource', 'git'); // TODO replace debug value
+
 Clock.init();
 
 Greeting.init();
+
+Wallpapers.init();
 
 //TODO rewrite with using  custom EventS!!!!!!! (changeLang and Time)
